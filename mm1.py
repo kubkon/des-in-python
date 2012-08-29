bd2f53eae568886151a693cda74dff90d870d9a1
Jakub Konka <kubkon@gmail.com>
2012-08-29 17:46:21 +0200

    Change license to MIT License

--- mm1.py
+++ mm1.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-mm1.py
-
-Created by Jakub Konka on 2012-08-22.
-Copyright (c) 2012 Jakub Konka.
-
-This library is free software; you can redistribute it and/or
-modify under the terms of the GNU General Public License as
-published by the Free Software Foundation; version 3.0.
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this library; if not, visit
-http://www.gnu.org/licenses/gpl-3.0.html
-"""
 
 import unittest
 import sim
