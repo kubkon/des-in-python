4fe57832401239b4220d834028674c85a17c03b8
Jakub Konka <kubkon@gmail.com>
2012-11-14 13:55:51 +0000

    Remove function main() from mm1_main.py.

--- mm1_main.py
+++ mm1_main.py
@@ -5,31 +5,26 @@ import mm1
 import sim
 
 
-def main():
-  ### Params
-  # Mean interarrival rate of customers per second;
-  # hence, 0.05 <=> 3 people/minute
-  interarrival_rate = 0.05
-  # Mean service rate by the teller per second;
-  # hence, 0.1 <=> 6 people/minute
-  service_rate = 0.1
+### Params
+# Mean interarrival rate of customers per second;
+# hence, 0.05 <=> 3 people/minute
+interarrival_rate = 0.05
+# Mean service rate by the teller per second;
+# hence, 0.1 <=> 6 people/minute
+service_rate = 0.1
   
-  ### Initialize
-  # Create new simulation engine
-  se = sim.SimulationEngine()
-  # Seed default PRNG
-  se.prng.seed = 100
-  # Create MM1 specific event handler
-  event_handler = mm1.MM1EventHandler()
-  event_handler.interarrival_rate = interarrival_rate
-  event_handler.service_rate = service_rate
+### Initialize
+# Create new simulation engine
+se = sim.SimulationEngine()
+# Seed default PRNG
+se.prng.seed = 100
+# Create MM1 specific event handler
+event_handler = mm1.MM1EventHandler()
+event_handler.interarrival_rate = interarrival_rate
+event_handler.service_rate = service_rate
   
-  ### Simulate
-  # Schedule finishing event; simulate for 24h
-  se.stop(60*60*24)
-  # Start simulating
-  se.start()
-
-
-if __name__ == '__main__':
-  main()
+### Simulate
+# Schedule finishing event; simulate for 24h
+se.stop(60*60*24)
+# Start simulating
+se.start()
