07f409bb6b8d008cf473aeb33fd0833dccfba402
Jakub Konka <kubkon@gmail.com>
2012-11-14 14:03:31 +0000

    Add command line arguments to main script.

--- mm1_main.py
+++ mm1_main.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import argparse
 import mm1
 import sim
+import time
 
+
+### Parse command line arguments
+parser = argparse.ArgumentParser(description="M/M/1 queue simulation -- Main script")
+parser.add_argument('sim_duration', metavar='simulation_duration',
+                    type=int, help='simulation duration in seconds')
+parser.add_argument('--seed', dest='seed', default=int(round(time.time())),
+                    type=int, help='seed for the PRNG (default: current system timestamp)')
+args = parser.parse_args()
+sim_duration = args.sim_duration
+seed = args.seed
 
 ### Params
 # Mean interarrival rate of customers per second;
@@ -17,14 +29,14 @@ service_rate = 0.1
 # Create new simulation engine
 se = sim.SimulationEngine()
 # Seed default PRNG
-se.prng.seed = 100
+se.prng.seed = seed
 # Create MM1 specific event handler
 event_handler = mm1.MM1EventHandler()
 event_handler.interarrival_rate = interarrival_rate
 event_handler.service_rate = service_rate
   
 ### Simulate
-# Schedule finishing event; simulate for 24h
-se.stop(60*60*24)
+# Schedule finishing event
+se.stop(sim_duration)
 # Start simulating
 se.start()
