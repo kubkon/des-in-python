0e44cae365259a7c1285f9e918ba5cf789a00def
Jakub Konka <kubkon@gmail.com>
2013-01-09 00:06:30 +0100

    Refactor tests for MM1EventHandler.
    Also, remove equality check for generated time of events---too brittle
    as depends on precise execution of the PRNG.

--- simulator/tests/mm1.py
+++ simulator/tests/mm1.py
@@ -9,8 +9,7 @@ from simulator.modules.sim import SimulationEngine, Event
 class MM1EventHandlerTests(unittest.TestCase):
   def setUp(self):
     sim = SimulationEngine()
-    self.seed = 0
-    sim.prng = np.random.RandomState(self.seed)
+    sim.prng = np.random.RandomState(0)
     self.eh = MM1EventHandler(sim)
     self.eh.interarrival_rate = 0.05
     self.eh.service_rate = 0.1
@@ -23,33 +22,33 @@ class MM1EventHandlerTests(unittest.TestCase):
     self.assertFalse(eh._is_processing)
 
   def test_properties(self):
-    self.eh.interarrival_rate = 0.2
-    self.eh.service_rate = 0.5
-    self.assertEqual(self.eh.interarrival_rate, 0.2)
-    self.assertEqual(self.eh.service_rate, 0.5)
+    self.assertEqual(self.eh.interarrival_rate, 0.05)
+    self.assertEqual(self.eh.service_rate, 0.1)
 
   def test_generate_arrival_event(self):
-    event = self.eh._generate_arrival_event(1.0)
-    prng = np.random.RandomState(self.seed)
+    event_time = 1.0
+    event = self.eh._generate_arrival_event(event_time)
     self.assertEqual(event.identifier, MM1EventHandler.ARRIVAL_EVENT)
-    self.assertEqual(event.time, 1.0 + prng.exponential(1/self.eh.interarrival_rate))
+    self.assertGreater(event.time, event_time)
 
   def test_generate_departure_event(self):
-    event = self.eh._generate_departure_event(1.0)
-    prng = np.random.RandomState(self.seed)
+    event_time = 1.0
+    event = self.eh._generate_departure_event(event_time)
     self.assertEqual(event.identifier, MM1EventHandler.DEPARTURE_EVENT)
-    self.assertEqual(event.time, 1.0 + prng.exponential(1/self.eh.service_rate))
+    self.assertGreater(event.time, event_time)
 
   def test_handle_arrival_event(self):
-    self.eh._handle_event(Event(MM1EventHandler.ARRIVAL_EVENT, 1.0))
+    event_time = 1.0
+    self.eh._handle_event(Event(MM1EventHandler.ARRIVAL_EVENT, event_time))
     self.assertEqual(self.eh._queue_length, 1)
-    self.assertEqual(self.eh._arrivals, [1.0])
+    self.assertEqual(self.eh._arrivals, [event_time])
 
   def test_handle_departure_event(self):
     self.eh._queue_length = 1
-    self.eh._handle_event(Event(MM1EventHandler.DEPARTURE_EVENT, 1.0))
+    event_time = 1.0
+    self.eh._handle_event(Event(MM1EventHandler.DEPARTURE_EVENT, event_time))
     self.assertEqual(self.eh._queue_length, 0)
-    self.assertEqual(self.eh._departures, [1.0])
+    self.assertEqual(self.eh._departures, [event_time])
 
   def test_is_processing_for_arrival_and_busy(self):
     self.eh._is_processing = True
